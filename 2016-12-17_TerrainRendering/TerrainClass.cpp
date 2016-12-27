2918169cc7660372cf0597a2df410681381ab926
inja2001m <dopa_1@naver.com>
2016-12-27 14:13:49 +0900

    Create TerrainClass

---
+++ 2016-12-17_TerrainRendering/TerrainClass.cpp
@@ -0,0 +1,219 @@
+#include "TerrainClass.h"
+
+TerrainClass::TerrainClass()
+{
+	m_vertexBuffer = 0;
+	m_indexBuffer = 0;
+}
+
+TerrainClass::TerrainClass(const TerrainClass &other)
+{
+}
+
+TerrainClass::~TerrainClass()
+{
+}
+
+bool TerrainClass::Initialize(ID3D11Device *device)
+{
+	bool result;
+
+	result = InitializeBuffers(device);
+	if (!result)
+		return false;
+
+	return true;
+}
+
+void TerrainClass::Shutdown()
+{
+	ShutdownBuffers();
+}
+
+bool TerrainClass::Render(ID3D11DeviceContext *deviceContext)
+{
+	RenderBuffers(deviceContext);
+
+	return true;
+}
+
+int TerrainClass::GetIndexCount()
+{
+	return m_indexCount;
+}
+
+bool TerrainClass::InitializeBuffers(ID3D11Device *device)
+{
+	VertexType *vertices;
+	unsigned long *indices;
+	D3D11_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
+	D3D11_SUBRESOURCE_DATA vertexData, indexData;
+	HRESULT result;
+	int i, j, terrainWidth, terrainHeight, index;
+	XMFLOAT4 color;
+	float positionX, positionZ;
+
+	terrainHeight = 256;
+	terrainWidth = 256;
+
+	color = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+
+	m_vertexCount = (terrainWidth - 1) * (terrainHeight - 1) * 8;
+	m_indexCount = m_vertexCount;
+
+	vertices = new VertexType[m_vertexCount];
+	if (!vertices)
+		return false;
+
+	indices = new unsigned long[m_indexCount];
+	if (!indices)
+		return false;
+
+	index = 0;
+
+	for (j = 0; j < terrainHeight - 1; ++j)
+	{
+		for (i = 0; i < terrainWidth - 1; ++i)
+		{
+			// Line 1 - Upper left
+			positionX = (float)i;
+			positionZ = (float)(j + i);
+
+			vertices[index].position = XMFLOAT3(positionX, 0.0f, positionZ);
+			vertices[index].color = color;
+			indices[index] = index;
+			++index;
+
+			// Line 1 - Upper right
+			positionX = (float)(i + 1);
+			positionZ = (float)(j + 1);
+
+			vertices[index].position = XMFLOAT3(positionX, 0.0f, positionZ);
+			vertices[index].color = color;
+			indices[index] = index;
+			++index;
+
+			// Line 2 - Upper right
+			positionX = (float)(i + 1);
+			positionZ = (float)(j + 1);
+
+			vertices[index].position = XMFLOAT3(positionX, 0.0f, positionZ);
+			vertices[index].color = color;
+			indices[index] = index;
+			++index;
+
+			// Line 2 - Bottom right
+			positionX = (float)(i + 1);
+			positionZ = (float)j;
+
+			vertices[index].position = XMFLOAT3(positionX, 0.0f, positionZ);
+			vertices[index].color = color;
+			indices[index] = index;
+			++index;
+
+			// Line 3 - Bottom right
+			positionX = (float)(i + 1);
+			positionZ = (float)j;
+
+			vertices[index].position = XMFLOAT3(positionX, 0.0f, positionZ);
+			vertices[index].color = color;
+			indices[index] = index;
+			++index;
+
+			// Line 3 - Bottom left
+			positionX = (float)i;
+			positionZ = (float)j;
+
+			vertices[index].position = XMFLOAT3(positionX, 0.0f, positionZ);
+			vertices[index].color = color;
+			indices[index] = index;
+			++index;
+
+			// Line 4 - Bottom left
+			positionX = (float)i;
+			positionZ = (float)j;
+
+			vertices[index].position = XMFLOAT3(positionX, 0.0f, positionZ);
+			vertices[index].color = color;
+			indices[index] = index;
+			++index;
+
+			// Line 4 - Upper left
+			positionX = (float)i;
+			positionZ = (float)(j + 1);
+
+			vertices[index].position = XMFLOAT3(positionX, 0.0f, positionZ);
+			vertices[index].color = color;
+			indices[index] = index;
+			++index;
+		}
+	}
+
+	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = sizeof(VertexType) * m_vertexCount;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.MiscFlags = 0;
+	vertexBufferDesc.StructureByteStride = 0;
+
+	vertexData.pSysMem = vertices;
+	vertexData.SysMemPitch = 0;
+	vertexData.SysMemSlicePitch = 0;
+
+	result = device->CreateBuffer(&vertexBufferDesc, &vertexData, &m_vertexBuffer);
+	if (FAILED(result))
+		return false;
+
+	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	indexBufferDesc.ByteWidth = sizeof(unsigned long) * m_indexCount;
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+	indexBufferDesc.StructureByteStride = 0;
+
+	indexData.pSysMem = indices;
+	indexData.SysMemPitch = 0;
+	indexData.SysMemSlicePitch = 0;
+
+	result = device->CreateBuffer(&indexBufferDesc, &indexData, &m_indexBuffer);
+	if (FAILED(result))
+		return false;
+
+	delete[] vertices;
+	vertices = NULL;
+
+	delete[] indices;
+	indices = NULL;
+
+	return true;
+}
+
+void TerrainClass::ShutdownBuffers()
+{
+	if (m_indexBuffer)
+	{
+		m_indexBuffer->Release();
+		m_indexBuffer = NULL;
+	}
+
+	if (m_vertexBuffer)
+	{
+		m_vertexBuffer->Release();
+		m_vertexBuffer = NULL;
+	}
+}
+
+void TerrainClass::RenderBuffers(ID3D11DeviceContext *deviceContext)
+{
+	unsigned int stride;
+	unsigned int offset;
+
+	stride = sizeof(VertexType);
+	offset = 0;
+
+	deviceContext->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
+	deviceContext->IASetIndexBuffer(m_indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
+
+	return;
+}
