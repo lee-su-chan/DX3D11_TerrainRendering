c15c087b6175e08d47d511976121a2434b190f28
inja2001m <dopa_1@naver.com>
2017-01-07 22:36:17 +0900

    Update TerrainShaderClass cpp, h

--- 2016-12-17_TerrainRendering/TerrainShaderClass.cpp
+++ 2016-12-17_TerrainRendering/TerrainShaderClass.cpp
@@ -42,6 +42,7 @@ bool TerrainShaderClass::Render(ID3D11DeviceContext *deviceContext,
 	XMMATRIX viewMatrix,
 	XMMATRIX projectionMatrix,
 	ID3D11ShaderResourceView *texture,
+	ID3D11ShaderResourceView *normalMap,
 	XMFLOAT3 lightDirection,
 	XMFLOAT4 diffuseColor)
 {
@@ -52,6 +53,7 @@ bool TerrainShaderClass::Render(ID3D11DeviceContext *deviceContext,
 		viewMatrix,
 		projectionMatrix,
 		texture,
+		normalMap,
 		lightDirection,
 		diffuseColor);
 
@@ -72,7 +74,7 @@ bool TerrainShaderClass::InitializeShader(ID3D11Device *device,
 	ID3D10Blob *errorMessage;
 	ID3D10Blob *vertexShaderBuffer;
 	ID3D10Blob *pixelShaderBuffer;
-	D3D11_INPUT_ELEMENT_DESC polygonLayout[4];
+	D3D11_INPUT_ELEMENT_DESC polygonLayout[6];
 	unsigned int numElements;
 	D3D11_BUFFER_DESC matrixBufferDesc;
 	D3D11_SAMPLER_DESC samplerDesc;
@@ -162,13 +164,29 @@ bool TerrainShaderClass::InitializeShader(ID3D11Device *device,
 	polygonLayout[2].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	polygonLayout[2].InstanceDataStepRate = 0;
 
-	polygonLayout[3].SemanticName = "COLOR";
+	polygonLayout[3].SemanticName = "TANGENT";
 	polygonLayout[3].SemanticIndex = 0;
 	polygonLayout[3].Format = DXGI_FORMAT_R32G32B32_FLOAT;
 	polygonLayout[3].InputSlot = 0;
 	polygonLayout[3].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
 	polygonLayout[3].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	polygonLayout[3].InstanceDataStepRate = 0;
+
+	polygonLayout[4].SemanticName = "BINORMAL";
+	polygonLayout[4].SemanticIndex = 0;
+	polygonLayout[4].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	polygonLayout[4].InputSlot = 0;
+	polygonLayout[4].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[4].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[4].InstanceDataStepRate = 0;
+
+	polygonLayout[5].SemanticName = "COLOR";
+	polygonLayout[5].SemanticIndex = 0;
+	polygonLayout[5].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	polygonLayout[5].InputSlot = 0;
+	polygonLayout[5].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[5].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[5].InstanceDataStepRate = 0;
 
 	numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
 
@@ -296,6 +314,7 @@ bool TerrainShaderClass::SetShaderParameters(ID3D11DeviceContext *deviceContext,
 	XMMATRIX viewMatrix,
 	XMMATRIX projectionMatrix,
 	ID3D11ShaderResourceView *texture,
+	ID3D11ShaderResourceView *normalMap,
 	XMFLOAT3 lightDirection,
 	XMFLOAT4 diffuseColor)
 {
@@ -330,6 +349,7 @@ bool TerrainShaderClass::SetShaderParameters(ID3D11DeviceContext *deviceContext,
 
 	deviceContext->VSSetConstantBuffers(bufferNumber, 1, &m_matrixBuffer);
 	deviceContext->PSSetShaderResources(0, 1, &texture);
+	deviceContext->PSSetShaderResources(1, 1, &normalMap);
 
 	result = deviceContext->Map(m_lightBuffer,
 		0,
