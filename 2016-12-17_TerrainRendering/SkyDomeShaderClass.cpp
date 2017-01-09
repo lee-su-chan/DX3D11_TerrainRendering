3f73d35f1bf5c78d04bc930c0793ac984e140348
inja2001m <dopa_1@naver.com>
2017-01-10 00:18:04 +0900

    Create SkyDomeShaderClass cpp, h

---
+++ 2016-12-17_TerrainRendering/SkyDomeShaderClass.cpp
@@ -0,0 +1,319 @@
+#include "SkyDomeShaderClass.h"
+
+SkyDomeShaderClass::SkyDomeShaderClass()
+{
+	m_vertexShader= 0;
+	m_pixelShader= 0;
+	m_layout= 0;
+	m_matrixBuffer= 0;
+	m_colorBuffer= 0;
+}
+
+SkyDomeShaderClass::SkyDomeShaderClass(const SkyDomeShaderClass &other)
+{
+}
+
+SkyDomeShaderClass::~SkyDomeShaderClass()
+{
+}
+
+bool SkyDomeShaderClass::Initialize(ID3D11Device *device, HWND hwnd)
+{
+	bool result;
+
+	result = InitializeShader(device, hwnd, L"skydome.vs", L"skydome.ps");
+	if (!result)
+		return false;
+
+	return true;
+}
+
+void SkyDomeShaderClass::Shutdown()
+{
+	ShutdownShader();
+
+	return;
+}
+
+bool SkyDomeShaderClass::Render(ID3D11DeviceContext *deviceContext,
+	int indexCount,
+	XMMATRIX worldMatrix,
+	XMMATRIX viewMatrix,
+	XMMATRIX projectionMatrix,
+	XMFLOAT4 apexColor,
+	XMFLOAT4 centerColor)
+{
+	bool result;
+
+	result = SetShaderParameters(deviceContext,
+		worldMatrix,
+		viewMatrix,
+		projectionMatrix,
+		apexColor,
+		centerColor);
+
+	if (!result)
+		return false;
+
+	RenderShader(deviceContext, indexCount);
+
+	return true;
+}
+
+bool SkyDomeShaderClass::InitializeShader(ID3D11Device *device,
+	HWND hwnd,
+	WCHAR *vsFilename,
+	WCHAR *psFilename)
+{
+	HRESULT result;
+	ID3D10Blob *errorMessage;
+	ID3D10Blob *vertexShaderBuffer;
+	ID3D10Blob *pixelShaderBuffer;
+	D3D11_INPUT_ELEMENT_DESC polygonLayout[1];
+	unsigned int numElements;
+	D3D11_BUFFER_DESC matrixBufferDesc;
+	D3D11_BUFFER_DESC colorBufferDesc;
+
+	errorMessage = 0;
+	vertexShaderBuffer = 0;
+	pixelShaderBuffer = 0;
+
+	result = D3DCompileFromFile(vsFilename,
+		NULL,
+		NULL,
+		"SkyDomeVertexShader",
+		"vs_5_0",
+		D3D10_SHADER_ENABLE_STRICTNESS,
+		0,
+		&vertexShaderBuffer,
+		&errorMessage);
+
+	if (FAILED(result))
+	{
+		if (errorMessage)
+			OutputShaderErrorMessage(errorMessage, hwnd, vsFilename);
+		else
+			MessageBox(hwnd, vsFilename, L"Missing Shader File", MB_OK);
+
+		return false;
+	}
+
+	result = D3DCompileFromFile(psFilename,
+		NULL,
+		NULL,
+		"SkyDomePixelShader",
+		"ps_5_0",
+		D3D10_SHADER_ENABLE_STRICTNESS,
+		0,
+		&pixelShaderBuffer,
+		&errorMessage);
+
+	if (FAILED(result))
+	{
+		if (errorMessage)
+			OutputShaderErrorMessage(errorMessage, hwnd, psFilename);
+		else
+			MessageBox(hwnd, psFilename, L"Missing Shader File", MB_OK);
+
+		return false;
+	}
+
+	result = device->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(),
+		vertexShaderBuffer->GetBufferSize(),
+		NULL,
+		&m_vertexShader);
+
+	if (FAILED(result))
+		return false;
+
+	result = device->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(),
+		pixelShaderBuffer->GetBufferSize(),
+		NULL,
+		&m_pixelShader);
+
+	if (FAILED(result))
+		return false;
+
+	polygonLayout[0].SemanticName = "POSITION";
+	polygonLayout[0].SemanticIndex = 0;
+	polygonLayout[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	polygonLayout[0].InputSlot = 0;
+	polygonLayout[0].AlignedByteOffset = 0;
+	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[0].InstanceDataStepRate = 0;
+
+	numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
+
+	result = device->CreateInputLayout(polygonLayout,
+		numElements,
+		vertexShaderBuffer->GetBufferPointer(),
+		vertexShaderBuffer->GetBufferSize(),
+		&m_layout);
+
+	if (FAILED(result))
+		return false;
+
+	vertexShaderBuffer->Release();
+	vertexShaderBuffer = NULL;
+
+	pixelShaderBuffer->Release();
+	pixelShaderBuffer = NULL;
+
+	matrixBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	matrixBufferDesc.ByteWidth = sizeof(MatrixBufferType);
+	matrixBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	matrixBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	matrixBufferDesc.MiscFlags = 0;
+	matrixBufferDesc.StructureByteStride = 0;
+
+	result = device->CreateBuffer(&matrixBufferDesc, NULL, &m_matrixBuffer);
+	if (FAILED(result))
+		return false;
+
+	colorBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	colorBufferDesc.ByteWidth = sizeof(ColorBufferType);
+	colorBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	colorBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	colorBufferDesc.MiscFlags = 0;
+	colorBufferDesc.StructureByteStride = 0;
+
+	result = device->CreateBuffer(&colorBufferDesc, NULL, &m_colorBuffer);
+	if (FAILED(result))
+		return false;
+
+	return true;
+}
+
+void SkyDomeShaderClass::ShutdownShader()
+{
+	if (m_colorBuffer)
+	{
+		m_colorBuffer->Release();
+		m_colorBuffer = NULL;
+	}
+
+	if (m_matrixBuffer)
+	{
+		m_matrixBuffer->Release();
+		m_matrixBuffer = NULL;
+	}
+
+	if (m_layout)
+	{
+		m_layout->Release();
+		m_layout = NULL;
+	}
+
+	if (m_pixelShader)
+	{
+		m_pixelShader->Release();
+		m_pixelShader = NULL;
+	}
+
+	if (m_vertexShader)
+	{
+		m_vertexShader->Release();
+		m_vertexShader = NULL;
+	}
+
+	return;
+}
+
+void SkyDomeShaderClass::OutputShaderErrorMessage(ID3D10Blob *errorMessage,
+	HWND hwnd,
+	WCHAR *shaderFilename)
+{
+	char *compileErrors;
+	unsigned __int64 bufferSize, i;
+	ofstream fout;
+
+	compileErrors = (char *)errorMessage->GetBufferPointer();
+	bufferSize = errorMessage->GetBufferSize();
+
+	fout.open("shader-error.txt");
+
+	for (i = 0; i < bufferSize; ++i)
+		fout << compileErrors[i];
+
+	fout.close();
+
+	errorMessage->Release();
+	errorMessage = NULL;
+
+	MessageBox(hwnd, L"Error compiling shader. Check shader-error.txt for message.", shaderFilename, MB_OK);
+
+	return;
+}
+
+bool SkyDomeShaderClass::SetShaderParameters(ID3D11DeviceContext *deviceContext,
+	XMMATRIX worldMatrix,
+	XMMATRIX viewMatrix,
+	XMMATRIX projectionMatrix,
+	XMFLOAT4 apexColor, 
+	XMFLOAT4 centerColor)
+{
+	HRESULT result;
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	MatrixBufferType *dataPtr;
+	unsigned int bufferNumber;
+	ColorBufferType *dataPtr2;
+
+	worldMatrix = XMMatrixTranspose(worldMatrix);
+	viewMatrix = XMMatrixTranspose(viewMatrix);
+	projectionMatrix = XMMatrixTranspose(projectionMatrix);
+
+	result = deviceContext->Map(m_matrixBuffer,
+		0,
+		D3D11_MAP_WRITE_DISCARD,
+		0,
+		&mappedResource);
+
+	if (FAILED(result))
+		return false;
+
+	dataPtr = (MatrixBufferType *)mappedResource.pData;
+
+	dataPtr->world = worldMatrix;
+	dataPtr->view = viewMatrix;
+	dataPtr->projection = projectionMatrix;
+
+	deviceContext->Unmap(m_matrixBuffer, 0);
+
+	bufferNumber = 0;
+
+	deviceContext->VSSetConstantBuffers(bufferNumber, 1, &m_matrixBuffer);
+
+	result = deviceContext->Map(m_colorBuffer,
+		0,
+		D3D11_MAP_WRITE_DISCARD,
+		0,
+		&mappedResource);
+
+	if (FAILED(result))
+		return false;
+
+	dataPtr2 = (ColorBufferType *)mappedResource.pData;
+
+	dataPtr2->apexColor = apexColor;
+	dataPtr2->centerColor = centerColor;
+
+	deviceContext->Unmap(m_colorBuffer, 0);
+
+	bufferNumber = 0;
+
+	deviceContext->PSSetConstantBuffers(bufferNumber, 1, &m_colorBuffer);
+
+	return true;
+}
+
+void SkyDomeShaderClass::RenderShader(ID3D11DeviceContext *deviceContext, int indexCount)
+{
+	deviceContext->IASetInputLayout(m_layout);
+	
+	deviceContext->VSSetShader(m_vertexShader, NULL, 0);
+	deviceContext->PSSetShader(m_pixelShader, NULL, 0);
+
+	deviceContext->DrawIndexed(indexCount, 0, 0);
+
+	return;
+}
