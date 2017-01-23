def5cd8257e4a2c651a4988a198473388621829b
inja2001m <dopa_1@naver.com>
2017-01-23 23:42:41 +0900

    Refair MiniMapClass

--- 2016-12-17_TerrainRendering/MinimapClass.cpp
+++ 2016-12-17_TerrainRendering/MinimapClass.cpp
@@ -42,7 +42,7 @@ bool MiniMapClass::Initialize(ID3D11Device *device,
 		screenHeight,
 		154,
 		154,
-		"data/minimap/minimap.tag");
+		"data/minimap/minimap.tga");
 	
 	if (!result)
 		return false;
@@ -55,9 +55,9 @@ bool MiniMapClass::Initialize(ID3D11Device *device,
 		deviceContext,
 		screenWidth,
 		screenHeight,
-		154,
-		154,
-		"data/minimap/point.tag");
+		3,
+		3,
+		"data/minimap/point.tga");
 
 	if (!result)
 		return false;
@@ -141,8 +141,8 @@ void MiniMapClass::PositionUpdate(float positionX, float positionZ)
 	percentX = positionX / m_terrainWidth;
 	percentY = 1.0f - positionZ / m_terrainHeight;
 
-	m_pointLocationX = (m_mapLocationX + 2) * (int)(percentX * m_mapSizeX);
-	m_pointLocationY = (m_mapLocationY + 2) * (int)(percentY * m_mapSizeY);
+	m_pointLocationX = (m_mapLocationX + 2) + (int)(percentX * m_mapSizeX);
+	m_pointLocationY = (m_mapLocationY + 2) + (int)(percentY * m_mapSizeY);
 
 	m_pointLocationX -= 1.0f;
 	m_pointLocationY -= 1.0f;
